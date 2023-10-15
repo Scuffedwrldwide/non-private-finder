699720cd3b66e4cd9f5f4ff525f07de1c1818cb3
SrGesus <108523575+SrGesus@users.noreply.github.com>
2023-10-15 03:04:03 +0100

    Removed useless code.

--- finder.py
+++ finder.py
@@ -1,4 +1,4 @@
-import re
+import regex
 import os
 
 # Path to the project directory to search for Java files
@@ -6,30 +6,6 @@ import os
 project_directory = '../'
 found = False
 print('\n')
-
-def find_non_private(java_code):
-    """
-    Finds non-private attributes in Java code.
-
-    Args:
-        java_code (str): The Java code to search for attributes.
-    Returns:
-        list: A list of tuples containing modifier, data type, attribute name, and line number.
-    """
-    # Regular expression to match non-private attributes
-    pattern = r'(public|protected|final|static|volatile|transient)\s+(\w+)\s+(\w+)\s*;'
-    lines = java_code.split('\n')
-
-    non_private_attributes = []
-
-    # Iterate through lines and extract attribute information
-    for i, line in enumerate(lines, start=1):
-        match = re.search(pattern, line)
-        if match:
-            modifier, data_type, attribute_name = match.groups()
-            non_private_attributes.append((modifier, data_type, attribute_name, i))
-
-    return non_private_attributes
 
 def process_java_file(file_path):
     """
@@ -42,20 +18,21 @@ def process_java_file(file_path):
         None
     """
     with open(file_path, 'r') as file:
+        global found
         java_code = file.read()
         relative_path = os.path.relpath(file_path, project_directory)
-        global found # Use global variable to indicate if any non-private attributes were found
-                     # I know this is awful, but I'm lazy. Fork and make a pull request about it.
-        class_name_match = re.search(r'class (\w+)', java_code)
-        if class_name_match:
-            class_name = class_name_match.group(1)
-            non_private_attributes = find_non_private(java_code)
-            num_attributes = len(non_private_attributes)
-            if num_attributes > 0:
+        outer_class_code = regex.findall(r'class .*\{([\S\s]*)\}', java_code)
+        class_code_with_inner = []
+        while len(outer_class_code):
+            class_code_with_inner += outer_class_code
+            outer_class_code += regex.findall(r'class .*\{([\S\s]*)\}', outer_class_code.pop())
+        for class_code in class_code_with_inner:
+            class_code_no_bracket = regex.sub(r'\{(?:[^{}]*|(?R))*\}', '', class_code)
+            for non_private_attributes in regex.findall(r'^((?!.*(abstract|private).*).*;)$', class_code_no_bracket, regex.M):
                 found = True
-                print(f"Class: \033[1m{class_name}\033[0m @ {relative_path} \033[33m\033[1m({num_attributes})\033[0m")
+                print(f"File: \033[1m{relative_path}\033[0m")
                 for attribute in non_private_attributes:
-                    print(f"  {attribute[0]} {attribute[1]} \033[1m{attribute[2]}\033[0m found in \033[33mline {attribute[3]}\033[0m\n")
+                    print(f"    \033[33m{attribute}\033[0m\n")
 
 # Walk through project directory and process each Java file, assumes there is a .git directory to avoid
 for root, dirs, files in os.walk(project_directory):
